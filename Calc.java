d6d57d0e7ac11b2277f36cb18569c370f23da3f0
safusust <safusust@gmail.com>
2024-04-24 16:52:47 +0600

    add screen for display

--- Calc.java
+++ Calc.java
@@ -7,7 +7,7 @@ class Calc {
         JButton nbtn[]=new JButton[10];
         JButton btnPlus, btnMinus, btnMultiply ,btnDivide,
         btnModulo,btnEqual,btn2Zero,btcAc, btnDel,btnPoint;
-        
+        Font ff= new Font("Ink.Free",Font.BOLD,30);
         public Calc(){
         frame= new JFrame();
         frame.setSize(400,600);
@@ -16,8 +16,15 @@ class Calc {
         frame.setTitle("CALCULATOR");
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.getContentPane().setBackground(new Color(100,100,100));
+
+
+        screen =new JTextField("Bangladesh");
+        screen.setBounds(25,25,325,50);
+       screen.setFont(ff);
+        frame.add(screen);
+        
+
         frame.setLayout(null);
         frame.setVisible(true);
 
